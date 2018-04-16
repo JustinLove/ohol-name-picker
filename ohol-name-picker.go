47d365ee44fd6284c7f6f55e58fdced9138c52f2
Justin Love <git@JustinLove.name>
2018-04-16 11:14:55 -0500

    read files in init, pick multiple names

--- ohol-name-picker.go
+++ ohol-name-picker.go
@@ -9,15 +9,29 @@ import (
 	"time"
 )
 
-func main() {
+const numberOfNames int = 10
+
+var firstNames []string
+var lastNames []string
+
+func init() {
 	rand.Seed(time.Now().UnixNano())
+	firstNames = loadNames("firstNames.txt")
+	lastNames = loadNames("lastNames.txt")
+}
 
-	firstNames := loadNames("firstNames.txt")
-	lastNames := loadNames("lastNames.txt")
-	first := firstNames[rand.Intn(len(firstNames))]
-	last := lastNames[rand.Intn(len(lastNames))]
+func main() {
+	var firsts [numberOfNames]string
+	var lasts [numberOfNames]string
 
-	fmt.Printf("%s %s", first, last)
+	for i := 0; i < numberOfNames; i++ {
+		firsts[i] = firstNames[rand.Intn(len(firstNames))]
+		lasts[i] = lastNames[rand.Intn(len(lastNames))]
+	}
+
+	for i := 0; i < numberOfNames; i++ {
+		fmt.Printf("%s %s\n", firsts[i], lasts[i])
+	}
 }
 
 func loadNames(filename string) ([]string) {
