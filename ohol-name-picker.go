a54113c6654f95ec8403ad1bef77e15571ebf11e
Justin Love <git@JustinLove.name>
2018-04-17 12:55:26 -0500

    working with default api gateway/lambda integration format

--- ohol-name-picker.go
+++ ohol-name-picker.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
@@ -17,6 +19,20 @@ type Names struct {
 	Lasts  [numberOfNames]string `json:"lasts"`
 }
 
+type Response struct {
+	//IsBase64Encoded bool              `json:"isBase64Encoded"`
+	StatusCode int `json:"statusCode"`
+	//Headers         map[string]string `json:"headers"`
+	Body string `json:"body"`
+}
+
+var errorResponse Response = Response{
+	//IsBase64Encoded: false,
+	StatusCode: 500,
+	//Headers:         make(map[string]string),
+	Body: "",
+}
+
 var firstNames []string
 var lastNames []string
 
@@ -26,25 +42,62 @@ func init() {
 	lastNames = loadNames("lastNames.txt")
 }
 
-func LambdaHandler() (Names, error) {
-	var response Names
+func pickNames() Names {
+	var names Names
 
 	for i := 0; i < numberOfNames; i++ {
-		response.Firsts[i] = firstNames[rand.Intn(len(firstNames))]
-		response.Lasts[i] = lastNames[rand.Intn(len(lastNames))]
+		names.Firsts[i] = firstNames[rand.Intn(len(firstNames))]
+		names.Lasts[i] = lastNames[rand.Intn(len(lastNames))]
+	}
+
+	return names
+}
+
+func LambdaHandler() (Response, error) {
+	names := pickNames()
+
+	jsonString, err := json.Marshal(names)
+	if err != nil {
+		return errorResponse, err
+	}
+
+	response := Response{
+		//IsBase64Encoded: true,
+		StatusCode: 200,
+		//Headers:         make(map[string]string),
+		Body: string(jsonString),
 	}
 
 	return response, nil
 }
 
-func main() {
-	lambda.Start(LambdaHandler)
-	return
-	response, _ := LambdaHandler()
+func printNames() {
+	names := pickNames()
 
 	for i := 0; i < numberOfNames; i++ {
-		fmt.Printf("%s %s\n", response.Firsts[i], response.Lasts[i])
+		fmt.Printf("%s %s\n", names.Firsts[i], names.Lasts[i])
 	}
+}
+
+func printResponse() {
+	response, lambdaErr := LambdaHandler()
+
+	jsonString, err := json.Marshal(response)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	os.Stdout.Write(jsonString)
+	log.Println(lambdaErr)
+}
+
+func main() {
+	lambda.Start(LambdaHandler)
+	//return
+
+	//printNames()
+	//printResponse()
 }
 
 func loadNames(filename string) []string {
